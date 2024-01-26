63a193bc95e8b3dd8bbde7298476c20988fe0ed7
Universidade Catolica de Brasilia <universidade@043ET082383.catolica.ubec>
2024-01-26 09:33:36 -0300

    adiciona uma nova atividade

---
+++ atividade64.py
@@ -0,0 +1,7 @@
+soma = qtd = 0
+while True :
+    numeros = int(input("insira o número: "))
+    if (numeros != 999):
+        soma += numeros
+        qtd += 1 
+        print ('Voce adicionou mais ')
